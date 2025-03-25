349d68cbe2d20299d652f9b2b2769a31b4d64d30
jalil-bend <bendellajalil2@gmail.com>
2025-03-25 14:10:32 +0100

    settings.py

--- medical_records/settings.py
+++ medical_records/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-pfqcbx4ch#hq1uh$be*cb3g+p2)tw=p7gtsy+b#ssbry2r+ktg'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
